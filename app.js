56f57952c97dfdd5a06ff3d664235fe1c2676b62
Olude david <odavidolumide@gmail.com>
2022-10-22 21:17:28 +0200

    calculator

---
+++ app.js
@@ -0,0 +1,103 @@
+//Step 1
+//add
+const add = (x, y) => {
+  return x + y;
+};
+
+//substract
+const substract = (x, y) => {
+  return x - y;
+};
+
+//divide
+const divide = (x, y) => {
+  return x / y;
+};
+
+//multiply
+const multiply = (x, y) => {
+  return x * y;
+};
+
+//step 2 operate function
+//two parameters: a. an operator b. two numbers
+//get user input
+let substractText = document.querySelector(".substract").textContent;
+//get user input, if a user clicks on the minus button or the plus or
+//div or mult value then we create a dynamic div and paragraph and then append the content of the user input
+//and get the content and store it in an array
+
+let userDigitsArraySetOne = [];
+let userOperatorVale = [];
+let operationV = [];
+
+//operator function
+const operate = (a, b) => {
+  if (a[0] == "-") {
+    return substract(b[0], b[1]);
+  }
+
+  if (a[0] == "+") {
+    return add(b[0], b[1]);
+  }
+  if (a[0] == "/") {
+    return divide(b[0], b[1]);
+  }
+
+  if (a[0] == "*") {
+    return multiply(b[0], b[1]);
+  }
+};
+let paraText = document.createElement("p");
+let buttons = document.querySelectorAll("button");
+buttons.forEach((elements) => {
+  if (elements.getAttribute("class") !== "equal-to") {
+    elements.addEventListener("click", function () {
+      let user_selection_one = document.querySelector(".user-selection-one");
+      let value = (user_selection_one.textContent += elements.textContent);
+      userDigitsArraySetOne.push(elements.textContent);
+      let joinedvalue = userDigitsArraySetOne.join("");
+      let operationValue = eval(joinedvalue);
+
+      document
+        .querySelector(".equal-to")
+        .addEventListener("click", function () {
+          user_selection_one.textContent = operationValue;
+          operationValue += elements.textContent;
+          operationV.push(operationValue);
+        });
+
+      document.querySelector(".clear").addEventListener("click", function () {
+        userDigitsArraySetOne = [];
+        user_selection_one.textContent = "";
+      });
+
+      // if (user_selection_one.textContent == operationValue) {
+      //   operationValue = elements.textContent;
+      //   userDigitsArraySetOne.push((operationValue += elements.textContent));
+      //   user_selection_one.textContent = eval(
+      //     (operationValue += elements.textContent)
+      //   );
+      // }
+
+      // if (elements.getAttribute("class") === "equal-to") {
+      //   elements.addEventListener("click", function () {
+      //     user_selection_one.textContent = operationValue;
+      //   });
+      // }
+      console.log(operationValue);
+      // // paraText.textContent = elements.textContent;
+      // // paraText.style.fontSize = "50px";
+      // // paraText.style.fontWeight = "300";
+      // displaysection.appendChild(paraText);
+
+      console.log(userDigitsArraySetOne);
+    });
+  }
+});
+
+let operatorButtons = document.querySelectorAll(".operators");
+
+operatorButtons.forEach((elements) => {
+  elements.addEventListener("click", function () {});
+});
